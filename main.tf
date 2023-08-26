976832b5520bfaea77459dcc8b8253c893101b4a
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-08-27 00:57:45 +0530

    vpc

--- main.tf
+++ main.tf
@@ -1,9 +1,5 @@
 module "components" {
-  source   = "git::https://github.com/lakshmiDev99/roboshop-module-test.git"
-  for_each = var.components
-
-  zone_id             = var.zone_id
-  security_groups     = var.security_groups
-  name                = each.value["name"]
-  instance_type       = each.value["instance_type"]
+  source   = "git::https://github.com/lakshmiDev99/tf-module-vpc.git"
+  for_each = var.vpc
+  cidr = each.value["cidr"]
 }
