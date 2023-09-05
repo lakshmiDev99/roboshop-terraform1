1a7447eb38944248bc2bea22def95a3ba235db70
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-05 16:01:27 +0530

    peering

--- env-dev/main.tfvars
+++ env-dev/main.tfvars
@@ -17,3 +17,6 @@ vpc={
     }
   }
 }
+default_vpc_id="vpc-04fe2aa33796ea49e"
+default_vpc_cidr="172.31.0.0/16"
+default_vpc_route_table_id="rtb-07f62759dea1db211"
--- main.tf
+++ main.tf
@@ -3,4 +3,11 @@ module "components" {
   for_each = var.vpc
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
+  default_vpc_id=var.default_vpc_id
+  default_vpc_cidr = var.default_vpc_cidr
+  default_vpc_route_table_id = var.default_vpc_route_table_id
+}
+
+output "vpc" {
+  value = "module.vpc"
 }
