f3a032d0e31c24d04212793a3c6f96b6ba56e43f
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-07 17:48:06 +0530

    load balancer

--- main.tf
+++ main.tf
@@ -4,8 +4,8 @@ module "vpc" {
   cidr = each.value["cidr"]
   subnets = each.value["subnets"]
   default_vpc_id=var.default_vpc_id
-  default_vpc_cidr = var.default_vpc_cidr
-  default_vpc_route_table_id = var.default_vpc_route_table_id
+#  default_vpc_cidr = var.default_vpc_cidr
+#  default_vpc_route_table_id = var.default_vpc_route_table_id
 #  tags = var.tags
 #  env = var.env
 }
