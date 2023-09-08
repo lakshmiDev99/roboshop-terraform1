64dda2edf6208af0ab064f3f8ac6521093948551
lakshmiDev99 <chakkalakshmi92@gmail.com>
2023-09-09 00:51:54 +0530

    load balancer

--- main.tf
+++ main.tf
@@ -26,7 +26,7 @@ module "alb" {
 
 module "docdb" {
   source   = "git::https://github.com/lakshmiDev99/tf-module-docdb.git"
-    tags            = var.tags
+  tags            = var.tags
   env             = var.env
   for_each        = var.docdb
   subnet_ids=local.db_subnets
